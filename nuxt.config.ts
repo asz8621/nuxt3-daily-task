a058dbe8ecdcdd7e1bd85930508810cb30226fe1
Nielsen <asz718460@gmail.com>
2024-11-21 20:05:06 +0800

    新增 Bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  css: [
+    '@/assets/stylesheets/all.scss', 
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: { // 在全部的元件都引入 _variables.scss
+          additionalData: `@import "@/assets/stylesheets/_variables.scss";`,
+        },
+      },
+    },
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
-<script setup>
-const route = useRoute();
-</script>
+<script setup></script>
 
 <template>
 	<div>
-		<h1>Page：首頁</h1>
-		<p>目前路由的路徑 - route.fullPath 為：{{ route.fullPath }}</p>
+		<h1>Page: index</h1>
+
+		<button type="button" class="btn btn-primary">Primary</button>
+		<button type="button" class="btn btn-secondary">Secondary</button>
+		<button type="button" class="btn btn-success">Success</button>
+		<button type="button" class="btn btn-danger">Danger</button>
+		<button type="button" class="btn btn-warning">Warning</button>
+		<button type="button" class="btn btn-info">Info</button>
+		<button type="button" class="btn btn-light">Light</button>
+		<button type="button" class="btn btn-dark">Dark</button>
+		<button type="button" class="btn btn-link">Link</button>
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+$primary: #0f6df2;
+h1 {
+	color: $primary;
+	margin-bottom: 20px;
+	font-size: $fs-xl;
+}
+</style>
 
