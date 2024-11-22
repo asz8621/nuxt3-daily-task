625041d41a05f7e7a6afb6dc14e8feb697ee8bae
Nielsen <asz718460@gmail.com>
2024-11-22 15:46:01 +0800

    修改 Header link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,38 +1,36 @@
 <script setup></script>
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <main class="container">
-    <slot />
-  </main>
+	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+		<div class="container">
+			<NuxtLink class="navbar-brand" to="/">Navbar</NuxtLink>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<NuxtLink class="nav-link" to="/">Home</NuxtLink>
+					</li>
+					<li class="nav-item">
+						<NuxtLink class="nav-link" to="/room">房型</NuxtLink>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<main class="container">
+		<slot />
+	</main>
 </template>
 
 <style scoped></style>
+
