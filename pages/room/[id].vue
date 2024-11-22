e8ef5594347804ae8571742e44996d1e25a25f00
Nielsen <asz718460@gmail.com>
2024-11-23 01:24:25 +0800

    將 room 修改為嵌套式路由

---
+++ pages/room/[id].vue
@@ -0,0 +1,18 @@
+<script setup>
+const route = useRoute();
+const router = useRouter();
+const goBack = () => {
+	router.go(-1);
+};
+</script>
+
+<template>
+	<h2>房型詳細頁面</h2>
+	<p>{{ route.fullPath }}</p>
+	<button type="button" class="btn btn-primary" @click="goBack">
+		回上一頁
+	</button>
+</template>
+
+<style scoped></style>
+
---
+++ pages/room/index.vue
@@ -0,0 +1,47 @@
+<script setup>
+const router = useRouter();
+const roomsList = ref([]);
+// 使用 fetch 或 axios 串接 前台房型 API ( GET )
+// apiUrl : https://nuxr3.zeabur.app/api/v1/rooms
+// response 回傳後，將資料寫入 roomsList 變數
+// 使用 roomsList 變數在下方 template 渲染列表
+const getRooms = async () => {
+	const response = await fetch('https://nuxr3.zeabur.app/api/v1/rooms');
+	const data = await response.json();
+	roomsList.value = data.result;
+};
+
+getRooms();
+</script>
+
+<template>
+	<div class="container mt-4">
+		<div class="row justify-content-center">
+			<div
+				class="col-8 col-md-6 col-lg-3"
+				v-for="item in roomsList"
+				:key="item._id"
+			>
+				<div
+					class="card h-100 shadow-sm"
+					@click="router.push(`/room/${item._id}`)"
+				>
+					<img :src="item.imageUrl" class="card-img-top" alt="Room Image" />
+					<div class="card-body d-flex flex-column">
+						<h3 class="card-title">{{ item.name }}</h3>
+						<p class="card-text flex-grow-1">{{ item.description }}</p>
+						<ul class="list-unstyled">
+							<li><strong>面積:</strong> {{ item.areaInfo }}</li>
+							<li><strong>床型:</strong> {{ item.bedInfo }}</li>
+							<li><strong>最大容納人數:</strong> {{ item.maxPeople }}</li>
+							<li><strong>價格:</strong> {{ item.price }}</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped></style>
+
